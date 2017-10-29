2dbe9e0730151e224b9554401137bfa6113e5be0
LoreenRH <lorena.rangel@docker.com>
2017-10-29 21:29:40 +0100

    Agregar menu súper básico
    Signed-off-by: LoreenRH <lorena.rangel@docker.com>

---
+++ menu.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+
+#include "suma.h"
+
+using namespace std;
+
+int main() {
+    int opt = 1;
+    do {
+        cout << "Selecciona una opcion" << endl;
+        cout << "1. Suma" << endl;
+        cout << "2. Resta" << endl;
+        cout << "3. Multiplicacion" << endl;
+        cout << "4. Division" << endl;
+        cout << "5. Salir " << endl;
+        
+        /*Agregar condiciones a continuacion... */
+
+    }while(opt!=5);
+    
+
+    return 0;
+}
